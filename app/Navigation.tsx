b4c3a891a58c53fb8e3fe15f5b6d3be312ea980a
Tejas Kamtam <tejaskamtam@yahoo.com>
2022-12-10 01:37:15 -0800

    setup next 13

---
+++ app/Navigation.tsx
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function Navigation() {
+  return (
+    <nav className="navbar">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Navbar
+        </a>
+      </div>
+    </nav>
+  );
+}
