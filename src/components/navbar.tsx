fdd2fcdba2959eada74a2b20c25a08b73bbe708d
Tejas Kamtam <tejaskamtam@yahoo.com>
2022-12-17 15:28:42 -0600

    adjusted navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,41 +2,63 @@ import React from "react";
 
 export default function () {
   return (
-    <div className="navbar bg-base-100">
-      <div className="flex-1">
-        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+    <div className="navbar bg-base-300">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          {/* sandwich menu */}
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 justify-items-end"
+          >
+            <li>
+              <a>About</a>
+            </li>
+            <li>
+              <a>Projects</a>
+            </li>
+            <li>
+              <a>Experience</a>
+            </li>
+            <li>
+              <a>API</a>
+            </li>
+          </ul>
+        </div>
+        <a className="btn btn-ghost normal-case text-xl">tejas kamtam</a>
       </div>
-      <div className="flex-none">
-        <ul className="menu menu-horizontal px-1">
+      {/* navbar menu */}
+      <div className="navbar-end hidden lg:flex mx-3">
+        <ul className="menu menu-horizontal px-1 mx-3">
           <li>
-            <a>Item 1</a>
-          </li>
-          <li tabIndex={0}>
-            <a>
-              Parent
-              <svg
-                className="fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                viewBox="0 0 24 24"
-              >
-                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-              </svg>
-            </a>
-            <ul className="p-2 bg-base-100">
-              <li>
-                <a>Submenu 1</a>
-              </li>
-              <li>
-                <a>Submenu 2</a>
-              </li>
-            </ul>
+            <a>About</a>
           </li>
           <li>
-            <a>Item 3</a>
+            <a>Projects</a>
+          </li>
+          <li>
+            <a>Experience</a>
+          </li>
+          <li>
+            <a>API</a>
           </li>
         </ul>
+        <a className="btn">résumé</a>
       </div>
     </div>
   );
