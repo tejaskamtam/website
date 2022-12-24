40daf004b024d5c513f8511bc8c06b44f17b8007
Tejas Kamtam <tejaskamtam@yahoo.com>
2022-12-23 20:30:17 -0600

    added displayname

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function () {
+export default function Navbar() {
   return (
     <div className="navbar bg-base-300">
       <div className="navbar-start">
